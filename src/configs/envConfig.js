f2d1611301d37a0a29a823018126faafe0311ca1
NguyenTruongVietBao <nguyentvbao2403@gmail.com>
2025-06-27 19:13:03 +0700

    update: env config

--- src/configs/envConfig.js
+++ src/configs/envConfig.js
@@ -1,6 +1,5 @@
 const envConfig = {
   BACKEND_URL: import.meta.env.VITE_BACKEND_URL,
-  FRONTEND_URL: import.meta.env.VITE_FRONTEND_URL,
   ACCESS_TOKEN_KEY: import.meta.env.VITE_ACCESS_TOKEN_KEY || 'accessToken',
   USER_KEY: import.meta.env.VITE_USER_KEY || 'user',
   AUTH_STORE_KEY: import.meta.env.VITE_AUTH_STORE_KEY || 'user-storage',
@@ -8,10 +7,6 @@ const envConfig = {
 
 if (!envConfig.BACKEND_URL) {
   throw new Error('BACKEND_URL is not defined');
-}
-
-if (!envConfig.FRONTEND_URL) {
-  throw new Error('FRONTEND_URL is not defined');
 }
 
 if (!envConfig.ACCESS_TOKEN_KEY) {
